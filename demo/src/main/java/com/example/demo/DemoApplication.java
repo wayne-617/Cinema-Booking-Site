7fd63289c1e933c63ef3eed582af6c01f141193e
Erich3310 <ceh36802@uga.edu>
2025-09-07 21:02:45 -0400

    Initial changes + live website display

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Welcome to AbsoluteCinema.com Beta 1.0";
+	}
+}
