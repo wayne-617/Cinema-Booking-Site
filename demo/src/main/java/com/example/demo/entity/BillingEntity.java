fc8689f0520ce55d9a55a210a703456c0e8bf96f
wayne-617 <wgl91603@uga.edu>
2025-10-12 23:53:45 -0400

    Add data.sql for mysql init

--- demo/src/main/java/com/example/demo/entity/BillingEntity.java
+++ demo/src/main/java/com/example/demo/entity/BillingEntity.java
@@ -3,38 +3,40 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
- @Entity
- @Table(name = "billing")
+@Entity
+@Table(name = "billing")
 public class BillingEntity {
-   
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long uid;
+
     @NotNull
     @Column(nullable = false, unique = true)
-    
     private String firstName;
+
     @NotNull
     @Column(nullable = false)
-
     private String lastName;
+
     @NotNull
     @Column(nullable = false)
-
     private String email;
+
     @NotNull
     @Column(nullable = false)
-
     private int last_four;
-    @Column(nullable = true)
 
+    // Add a foreign key reference to the UserEntity
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false) // This will be the foreign key column
+    private UserEntity user;  // Reference to the UserEntity
 
     public long getUid() {
         return uid;
     }
 
-    public String getFirstName(){
+    public String getFirstName() {
         return firstName;
     }
 
@@ -48,6 +50,10 @@ public class BillingEntity {
 
     public int getCardInfo() {
         return last_four;
+    }
+
+    public UserEntity getUser() {
+        return user;
     }
 
     public void setUid(long uid) {
@@ -66,7 +72,11 @@ public class BillingEntity {
         this.email = email;
     }
 
-    public void setCardInfo(int last_four){
+    public void setCardInfo(int last_four) {
         this.last_four = last_four;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
     }
 }
